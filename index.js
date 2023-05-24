a1c54918ea5d79ee409031934f22d63e7fcbbfcf
Travis <travis.williams94@gmail.com>
2023-05-24 14:01:41 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    const p = document.querySelector('p');
+    p.textContent = 'This is really cool!'
+});
+
+
+
+
+
